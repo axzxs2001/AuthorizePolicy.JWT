20494577ceefbad1405b3e95ee0f5a6a53b9fc55
桂素伟 <axzxs2001@163.com>
2017-09-15 22:58:12 +0800

    创建AuthorizePolicy.JWT和测试web api和客户端

---
+++ AuthorizePolicy.JWT/AuthorizePolicy.JWT.Test/Controllers/PermissionController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
+using AuthorizePolicy.JWT;
+
+namespace Token_WebAPI01.Controllers
+{
+    [Authorize("Permission")]
+    public class PermissionController : Controller
+    {
+        /// <summary>
+        /// 自定义策略参数
+        /// </summary>
+        PermissionRequirement requirement;
+        public PermissionController(IAuthorizationHandler authorizationHander)
+        {
+            requirement = (authorizationHander as PermissionHandler).Requirement;
+        }
+        [AllowAnonymous]
+        [HttpPost("/api/login")]
+        public IActionResult Login(string username,string password,string role)
+        { 
+            var isValidated = username == "gsw" && password == "111111";
+            if (!isValidated)
+            {
+                return new JsonResult(new
+                {
+                    Status = false,
+                    Message = "认证失败"
+                });
+            }
+            else
+            {
+                //用户标识
+                var identity = new ClaimsIdentity(JwtBearerDefaults.AuthenticationScheme);
+                //如果是基于角色的授权策略，这里要添加用户
+                identity.AddClaim(new Claim(ClaimTypes.Name, username));
+                //如果是基于角色的授权策略，这里要添加角色
+                identity.AddClaim(new Claim(ClaimTypes.Role, role));
+                HttpContext.SignInAsync(JwtBearerDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+                var token = GetJwt(username, role);
+                return new JsonResult(token);
+            }
+        }
+        [AllowAnonymous]
+        [HttpGet("/api/denied")]
+        public IActionResult Denied()
+        {
+            return new JsonResult(new
+            {
+                Status = false,
+                Message = "你无权限访问"
+            });
+        }
+
+
+        /// <summary>
+        /// get the jwt
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        private dynamic GetJwt(string username,string role)
+        {
+            var now = DateTime.UtcNow;
+            var claims = new Claim[]
+            {        
+                //用户名
+                new Claim(ClaimTypes.Name,username),
+                //角色
+                new Claim(ClaimTypes.Role,role)
+            };
+            var jwt = new JwtSecurityToken(
+                issuer: requirement.Issuer,
+                audience: requirement.Audience,
+                claims: claims,
+                notBefore: now,
+                expires: now.Add(requirement.Expiration),
+                signingCredentials: requirement.SigningCredentials
+            );
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+            var response = new
+            {
+                Status = true,
+                access_token = encodedJwt,
+                expires_in = requirement.Expiration.TotalMilliseconds,
+                token_type = "Bearer"
+            };
+            return response;
+        }
+
+    }
+}
---
+++ AuthorizePolicy.JWT/AuthorizePolicy.JWT.Test/Startup.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthorizePolicy.JWT.Test
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+     
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var audienceConfig = Configuration.GetSection("Audience");
+            var symmetricKeyAsBase64 = audienceConfig["Secret"];
+            var keyByteArray = Encoding.ASCII.GetBytes(symmetricKeyAsBase64);
+            var signingKey = new SymmetricSecurityKey(keyByteArray);
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+                ValidateIssuer = true,
+                ValidIssuer = audienceConfig["Issuer"],
+                ValidateAudience = true,
+                ValidAudience = audienceConfig["Audience"],
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+            var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+            services.AddAuthorization(options =>
+            {
+                //这个集合模拟用户权限表,可从数据库中查询出来
+                var permission = new List<Permission> {
+                              new Permission {  Url="/", Name="admin"},
+                              new Permission {  Url="/api/values", Name="admin"},
+                              new Permission {  Url="/", Name="system"},
+                              new Permission {  Url="/api/values1", Name="system"}
+                          };
+                //如果第三个参数，是ClaimTypes.Role，上面集合的每个元素的Name为角色名称，如果ClaimTypes.Name，即上面集合的每个元素的Name为用户名
+                var permissionRequirement = new PermissionRequirement("/api/denied", permission, ClaimTypes.Role, audienceConfig["Issuer"], audienceConfig["Audience"], signingCredentials);
+                options.AddPolicy("Permission",
+                          policy => policy.Requirements.Add(permissionRequirement));
+            }).AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(o =>
+            {
+                //不使用https
+                o.RequireHttpsMetadata = false;
+                o.TokenValidationParameters = tokenValidationParameters;
+            });
+            //注入授权Handler
+            services.AddSingleton<IAuthorizationHandler, PermissionHandler>();
+            services.AddMvc();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
