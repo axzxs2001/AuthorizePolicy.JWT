5d857d0014ceee83a2b416e2e0e21f1487fc9dfd
桂素伟 <axzxs2001@163.com>
2017-10-31 14:08:21 +0800

    完善注释。

--- AuthorizePolicy.JWT/AuthorizePolicy.JWT.Test/Controllers/PermissionController.cs
+++ AuthorizePolicy.JWT/AuthorizePolicy.JWT.Test/Controllers/PermissionController.cs
@@ -38,9 +38,8 @@ namespace Token_WebAPI01.Controllers
                 });
             }
             else
-            {
-               
-                //如果是基于角色的授权策略，这里要添加用户;如果是基于角色的授权策略，这里要添加角色
+            {               
+                //如果是基于用户的授权策略，这里要添加用户;如果是基于角色的授权策略，这里要添加角色
                 var claims = new Claim[] { new Claim(ClaimTypes.Name, username), new Claim(ClaimTypes.Role, role), new Claim(ClaimTypes.Expiration ,DateTime.Now.AddSeconds(_requirement.Expiration.TotalSeconds).ToString())};
                 //用户标识
                 var identity = new ClaimsIdentity(JwtBearerDefaults.AuthenticationScheme);
